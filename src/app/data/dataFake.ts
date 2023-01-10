54f29d6a79a9d5719330fb8f1c19feff8b0974fd
olgajuanne <olgajdango@gmail.com>
2023-01-10 20:15:35 -0300

    Page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,33 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id":"1",
+    "title": "Filme Jujutsu Kaisen 0",
+    "description": "“Yuta Okkotsu é um estudante nervoso que está sofrendo de um problema sério — sua amiga de infância Rika se transformou em uma maldição e não o deixa em paz. Já que Rika não é uma maldição comum, sua situação é notada por Satoru Gojo, um professor da Jujutsu High, uma escola onde exorcistas novatos aprendem como combater as maldições. Gojo convence Yuta a se inscrever, mas ele pode aprender o suficiente a tempo de enfrentar a maldição que o assombra?”",
+    "photoCover": "https://semprefuigeek.com.br/wp-content/uploads/2022/03/Jujutsu-Kaisen-0.jpg"
   },
   {
-    "id":2,
-    "photo": "bbb",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id":"2",
+    "title": "Lançado o primeiro teaser da segunda temporada de JUJUTSU KAISEN",
+    "description": "A conta oficial do Twitter de JUJUTSU KAISEN compartilhou o primeiro teaser visual da segunda temporada, que apresenta o todo-poderoso Satoru Gojo e o antagonista de JUJUTSU KAISEN 0 , Suguru Geto. A segunda temporada de JUJUTSU KAISEN está programada para ser transmitida em algum momento de 2023. O novo teaser visual apresenta Gojo, Geto e seu colega de classe Shoko Ieiri, vistos em seus dias de estudantes, com um quarto personagem desconhecido cuja identidade é obscurecida por um conjunto de dedos na parte superior do quadro que, à primeira vista, parece ser um rastro sinistro de sangue escorrendo do título da série.",
+    "photoCover": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/09/JJK-SEASON-2-FIRST-TEASER.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
+  },
+  {
+    "id":"3",
+    "title": "Dr. Stone: O que esperar da 3ª temporada (de acordo com o mangá)",
+    "description": "Dr. Stone: New World , o nome oficial da 3ª temporada, começará com o Arco Age of Exploration , embora esse arco já tenha começado. Os eventos contados no último episódio de Dr. Stone: Stone Wars e no especial Dr. Stone: Ryusui fazem parte desse arco no mangá. Depois de acabar com Tsukasa e vencer (sem muita disputa) a competição de design de barcos , Senku precisava de um capitão. Graças às informações que Minami lhes deu , eles conseguiram encontrar Ryusui, um homem que já foi filho de uma família incrivelmente rica, mas também era verdadeiramente apaixonado por vedar e operar barcos de todos os tipos a ponto de viajar constantemente por aí. o mundo com eles. No final da especial conseguiram completar com sucesso uma viagem de balão de ar quente para ajudar a encontrar petróleo após a grande mudança de paisagem para fazer funcionar um motor, construir parte do barco e estabelecer uma moeda, os Dragos. No entanto, a próxima coisa na fila é garantir que eles tenham comida suficiente para uma longa jornada.",
+    "photoCover": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/12/untitled-design-9.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
+  },
+  {
+    "id":"4",
+    "title": "Spy X Family: 2ª temporada e filme anunciados",
+    "description": "A adaptação para anime de grande sucesso do mangá Spy x Family de Tatsuya Endo está ganhando uma segunda temporada, bem como um filme original. No evento Jump Festa 2023 , em 18 de dezembro de 2022, foi anunciado que o anime SPY X FAMILY ganhará uma segunda temporada, bem como um filme teatral, ambos com lançamento previsto para 2023. Foi ainda dito que a 2ª temporada da série de anime está atualmente em produção. Também foi revelado que o criador do mangá original, Tatsuya Endo, será responsável pelo roteiro do filme, design dos personagens e supervisão. O filme SPY X FAMILY também contará com novos personagens.",
+    "photoCover": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/12/spy-x-family-the-movie-cover-news.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
+  },
+  {
+    "id":"5",
+    "title": "",
+    "description": "Descrição do conteudo: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni omnis sit dicta pariatur atque eveniet recusandae, voluptatem, earum sequi odio modi cum fugiat laborum consequatur ratione quaerat iste laboriosam explicabo.",
+    "photoCover": ""
   }
 
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,17 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit{
   photoCover:string= ""
-  contentTitle:string= "Noticia exemplo"
-  contentDescription:string= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci excepturi, rerum commodi reprehenderit praesentium nam consequatur quidem voluptatem iste quia consequuntur architecto quod autem! Obcaecati possimus ut totam velit omnis!"
+  contentTitle:string= ""
+  contentDescription:string= ""
+  private id:string | null ="0"
 
   constructor(
     private route:ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
-    )  
+      this.id = value.get("id")
+    ) 
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+    
+     
+       this.contentTitle = result.title
+       this.contentDescription = result.description
+       this.photoCover = result.photoCover
+     
   }
 
 }
